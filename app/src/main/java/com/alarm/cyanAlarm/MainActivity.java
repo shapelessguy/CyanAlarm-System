8c6c905061e4cecde2882a4592011447217822c2
Claudio Ciano <shapelessguy@gmail.com>
2021-12-31 16:44:53 +0100

    first commit

--- app/src/main/java/com/alarm/cyanAlarm/MainActivity.java
+++ app/src/main/java/com/alarm/cyanAlarm/MainActivity.java
@@ -50,7 +50,6 @@ import android.widget.TextView;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
-import java.time.format.DateTimeFormatter;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.List;
